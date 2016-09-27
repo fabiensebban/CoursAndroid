2b3e8ebd196152a9f782be3329ee887911b9fe08
Fabian Sebban <fa.sebban@gmail.com>
2016-09-27 12:04:20 +0200

    27-09 - Exercice 6

--- app/src/main/java/com/example/fabiansebban/firstproject/MainActivity.java
+++ app/src/main/java/com/example/fabiansebban/firstproject/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.fabiansebban.firstproject;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -13,6 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mainListView;
+    private Button mainButtonView;
 
     // Permet d'initialiser l'appli
     @Override
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_ex4);
 
         mainListView = (ListView)findViewById(R.id.toto);
+        mainButtonView = (Button)findViewById(R.id.confirmButton);
 
         ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice);
 
@@ -42,6 +48,27 @@ public class MainActivity extends AppCompatActivity {
 
         mainListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         mainListView.setAdapter(listAdapter);
+
+        mainButtonView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                int i = 0;
+                String toPrint = "";
+
+                SparseBooleanArray checkedElements = mainListView.getCheckedItemPositions();
+
+                while(i < mainListView.getAdapter().getCount())
+                {
+                    if(checkedElements.get(i))
+                    {
+                        toPrint = toPrint + mainListView.getAdapter().getItem(i) + "\n";
+                    }
+
+                    i++;
+                }
+                Log.d("Mes logs", toPrint);
+            }
+        });
     }
 
     // Délancher quand l'utilisateur quitte l'appli
