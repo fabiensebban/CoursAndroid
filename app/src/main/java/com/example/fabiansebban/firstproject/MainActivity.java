f2bd63a3daed6dc18579cddcb2e2c46c14067676
Fabian Sebban <fa.sebban@gmail.com>
2016-09-27 10:24:24 +0200

    27-09 - Exercice 1 et 2

--- app/src/main/java/com/example/fabiansebban/firstproject/MainActivity.java
+++ app/src/main/java/com/example/fabiansebban/firstproject/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.fabiansebban.firstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mainListView;
 
     // Permet d'initialiser l'appli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_ex4);
+        mainListView = (ListView)findViewById(R.id.simple_list_item_1);
+
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), listAdapter.getItem(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listAdapter.add("First");
+        listAdapter.add("Second");
+        listAdapter.add("Third");
+
+
+        mainListView.setAdapter(listAdapter);
     }
 
     // Délancher quand l'utilisateur quitte l'appli
